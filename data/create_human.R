3576f499e92ea66bb9caf01903af0840f37a39f4
Aapo Virtanen <aapo@Aapo-MBP.fritz.box>
2022-12-05 18:14:31 +0100

    submit

--- data/create_human.R
+++ data/create_human.R
@@ -5,53 +5,41 @@
 
 #Downloading data 
 
-#install.packages("readr")
+install.packages("readr")
 library(readr)
-hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
-gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv",na = "..")
-dim(hd)
-#The Human development index data has 195 rows and 8 columns
-
-dim(gii)
-#The gender inequality data has 195 rows and 10 columns
-
-library(dplyr)
-library(tidyverse)
-
-#Looking at the names of the variables in the hd data frame
-names(hd)
-#Original variable names: 
-#"HDI Rank"                               "Country"                                "Human Development Index (HDI)"         
-#"Life Expectancy at Birth"               "Expected Years of Education"            "Mean Years of Education"               
-#"Gross National Income (GNI) per Capita" "GNI per Capita Rank Minus HDI Rank"    
-
-hd<-rename(hd, GNI = "Gross National Income (GNI) per Capita", Life.Exp = "Life Expectancy at Birth", Edu.Exp = "Expected Years of Education")
-
-names(gii)
-#[1] "GII Rank"                                     "Country"                                      "Gender Inequality Index (GII)"               
-#"Maternal Mortality Ratio"                     "Adolescent Birth Rate"                        "Percent Representation in Parliament"        
-#"Population with Secondary Education (Female)" "Population with Secondary Education (Male)"   "Labour Force Participation Rate (Female)"    
-#"Labour Force Participation Rate (Male)"  
-
-gii<-rename(gii, Mat.Mor="Maternal Mortality Ratio", Ado.Birt="Adolescent Birth Rate", Parli.F="Percent Representation in Parliament", 
-       Edu2.F="Population with Secondary Education (Female)", Edu2.M="Population with Secondary Education (Male)",Labo.F="Labour Force Participation Rate (Female)",
-       Labo.M="Labour Force Participation Rate (Male)")
-
-class(gii$Edu2.F)
-
-#Adding new columns with mutate 
 library(dplyr)
 library(tidyverse)
 library(scales)
-gii<-mutate(gii, Edu.2FM = Edu2.F/Edu2.M, Labo.FM = Labo.F/Labo.M)
-
+hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
+gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv",na = "..")
+hd<-rename(hd, GNI = "Gross National Income (GNI) per Capita", Life.Exp = "Life Expectancy at Birth", 
+           Edu.Exp = "Expected Years of Education")
+gii<-rename(gii, Mat.Mor="Maternal Mortality Ratio", Ado.Birth="Adolescent Birth Rate", 
+            Parli.F="Percent Representation in Parliament", 
+       Edu2.F="Population with Secondary Education (Female)", Edu2.M="Population with Secondary Education (Male)",
+       Labo.F="Labour Force Participation Rate (Female)",
+       Labo.M="Labour Force Participation Rate (Male)")
+gii<-mutate(gii, Edu2.FM = Edu2.F/Edu2.M, Labo.FM = Labo.F/Labo.M)
 #Joining data sets
 human <- inner_join(gii, hd, by = "Country", suffix = c(".gii", ".hd"))
 dim(human)
-
-#The new joined data set has 195 rows or observations and 19 columns or variables including the 2 new variables
-
-#Saving the new data
+#The  joined data  has 195 rows or observations and 19 columns 
+#Transform the Gross National Income (GNI) variable to numeric (using string manipulation). 
+human$GNI<-as.numeric(human$GNI)
+#Exclude unneeded variables:
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- select(human, one_of(keep))
+#Remove all rows with missing values.
+human<-na.omit(human)
+#Remove the observations which relate to regions instead of countries
+last<-tail(human)
+last <- nrow(human) - 7
+human <- human[1:last,]
+#Define the row names of the data by the country names and remove the country name column from the data. 
+human<-as.data.frame(human)
+rownames(human) <- human$Country
+human<- select(human, -Country)
+#str(human)
+#View(human)
 write_csv(human, "data/human.csv")
-
 
